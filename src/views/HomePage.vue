e8cc2931b62b639c71db21ea992a68848759b67a
Lasicak1 <93720915+Lasicak1@users.noreply.github.com>
2022-02-01 23:51:47 +0100

    commit

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,28 +1,70 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
+        <!-- <img src="https://i.ibb.co/dkTnR4B/Logo-Nav-Bar-1.png" alt="Logo-Nav-Bar-1" border="0">
+        <h3>Info?</h3> -->
       </ion-header>
-    
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+              <img class="Logo" src="https://i.ibb.co/dkTnR4B/Logo-Nav-Bar-1.png" alt="Space-Logo" border="0">
+          </ion-col>
+          <ion-col>
+            <br>
+          </ion-col>
+          <ion-col>
+            <div class="Menu">
+                <img src="https://i.ibb.co/gFMJwTn/More-Icon.png" alt="Hamburger-Menu" border="0">
+            </div>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      
+        <ion-grid>
+            <ion-row>
+              <br>
+            </ion-row>
+              <div class="Text">
+                  <h1>Software studio</h1>
+                  <h1 class="Text2">founded by three</h1>
+                  <h1 class="Gradient-Text">highschool students.</h1>             
+              </div>
+            <ion-row>
+                <div class="Technologies">
+                    <ion-col>
+                        <img src="https://i.ibb.co/tx1gK9Z/October-CMS.png" alt="October-CMS" border="0">
+                    </ion-col>
+                    <ion-col></ion-col>
+                    <ion-col>
+                        <img src="https://i.ibb.co/yRqx2PY/Vue.png" alt="Vue" border="0"> 
+                    </ion-col>
+                    <ion-col></ion-col>
+                    <ion-col> 
+                        <img src="https://i.ibb.co/RvGsjKm/AWS.png" alt="AWS" border="0">
+                    </ion-col>
+                    <ion-col></ion-col>
+                    <ion-col> 
+                        <img src="https://i.ibb.co/4TrzSsz/Ionic.png" alt="Ionic" border="0">
+                    </ion-col>
+                </div>
+            </ion-row>
+            <div class="Team">
+                <img src="https://i.ibb.co/PNwf3JX/Team.png" alt="Team" border="0">
+            </div>
+              
+
+        </ion-grid>
+
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonGrid} from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -30,13 +72,17 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
+    IonGrid,
   }
 });
 </script>
 
 <style scoped>
+
+#Text{
+  margin-top: 1vh;
+}
+
 #container {
   text-align: center;
   
@@ -56,7 +102,7 @@ export default defineComponent({
   font-size: 16px;
   line-height: 22px;
   
-  color: #8c8c8c;
+  color: #fffbfb;
   
   margin: 0;
 }
@@ -64,4 +110,49 @@ export default defineComponent({
 #container a {
   text-decoration: none;
 }
+
+.Logo{
+  margin-left: 1vw;
+  margin-top: 3vh;
+}
+.Menu{
+  position: absolute;
+  margin-top: 3.5vh;
+  right: 7vw;
+}
+.Text{
+  text-align: center;
+}
+.Text2{
+  margin-top: 0vh;
+}
+
+.Gradient-Text{
+  margin-top: 0vh;
+  background: -webkit-linear-gradient(360deg, #00D1FF 40%, #0209B5 85%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+
+.Technologies{
+  margin-top: 5vh;
+  margin: auto;
+}
+
+.Team{
+  margin-top: 4vh;
+  margin: auto;
+  width: 223px;
+  height: 223px;
+}
+
+
+h1{
+  font-size: 31px;
+  font-family: 'Helvetica';
+  font-weight: 800;
+}
+
+
 </style>
