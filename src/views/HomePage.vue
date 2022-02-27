5756ee858ee6666964a4a57b4e1c7b8cd5a0abe6
Lasicak1 <martinsimontkac@gmail.com>
2022-02-27 10:57:02 +0100

    commit for testing

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,20 +1,8 @@
 <template>
   <ion-page>
-
-    <!-- <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header> -->
-
-
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <!-- <img src="https://i.ibb.co/dkTnR4B/Logo-Nav-Bar-1.png" alt="Logo-Nav-Bar-1" border="0">
-        <h3>Info?</h3> -->
-      </ion-header>
 
-
+    <!-- header -->     
       <ion-grid>
         <ion-row>
           <ion-col>
@@ -31,6 +19,7 @@
         </ion-row>
       </ion-grid>
       
+      <!-- software studio text -->
         <ion-grid>
             <ion-row>
               <br>
@@ -40,29 +29,48 @@
                   <h1 class="Text2">founded by three</h1>
                   <h1 class="Gradient-Text">highschool students.</h1>             
               </div>
+
+            <!-- technologie logos   -->
             <ion-row>
                 <div class="Technologies">
-                    <ion-col>
+                    <ion-col class="CMS">
                         <img src="https://i.ibb.co/tx1gK9Z/October-CMS.png" alt="October-CMS" border="0">
                     </ion-col>
                     <ion-col></ion-col>
-                    <ion-col>
+                    <ion-col class="Vue">
                         <img src="https://i.ibb.co/yRqx2PY/Vue.png" alt="Vue" border="0"> 
                     </ion-col>
                     <ion-col></ion-col>
-                    <ion-col> 
+                    <ion-col class="AWS"> 
                         <img src="https://i.ibb.co/RvGsjKm/AWS.png" alt="AWS" border="0">
                     </ion-col>
                     <ion-col></ion-col>
-                    <ion-col> 
+                    <ion-col class="Ionic"> 
                         <img src="https://i.ibb.co/4TrzSsz/Ionic.png" alt="Ionic" border="0">
                     </ion-col>
                 </div>
+
+            <!-- team picture -->
             </ion-row>
             <div class="Team">
                 <img src="https://i.ibb.co/PNwf3JX/Team.png" alt="Team" border="0">
             </div>
-              
+            
+            <!-- About us Text -->
+            <ion-row>
+              <div class="About-Us">
+                <h1>About Us</h1>
+              </div>
+            </ion-row>
+
+            <!-- Bottom Text -->
+            <ion-row>
+              <div class="Bottom-Text">
+                <h2>We are new sofware studio founded by 3 highschool students from Slovakia.
+Currently we are studying on high school SPŠE Hálova 16.
+And we are part of really cool study plan named OPENLAB.</h2>
+              </div>
+            </ion-row>
 
         </ion-grid>
 
@@ -73,13 +81,12 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonGrid, IonRow, IonCol, } from '@ionic/vue';
+import { IonContent, IonPage, IonGrid, IonRow, IonCol, } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   components: {
     IonContent,
-    IonHeader,
     IonPage,
     IonGrid,
     IonRow,
@@ -96,7 +103,6 @@ export default defineComponent({
 
 #container {
   text-align: center;
-
   position: absolute;
   left: 0;
   right: 0;
@@ -136,7 +142,6 @@ export default defineComponent({
 .Text2{
   margin-top: 0vh;
 }
-
 .Gradient-Text{
   margin-top: 0vh;
   background: -webkit-linear-gradient(360deg, #00D1FF 40%, #0209B5 85%);
@@ -144,19 +149,43 @@ export default defineComponent({
   -webkit-text-fill-color: transparent;
   background-clip: text;
 }
-
 .Technologies{
-  margin-top: 5vh;
-  margin: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2vh;
 }
-
 .Team{
-  margin-top: 4vh;
-  margin: auto;
-  width: 223px;
-  height: 223px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 5vh;
+  width: 300px;
+  height: 300px;
 }
 
+.Vue{
+  margin-left: 5vw;
+}
+
+.AWS{
+ margin-left: 6vw;
+}
+
+.Ionic{
+ margin-left: 7vw;
+}
+
+.About-Us{
+  margin-top: 0vh;
+  color: 2F2F2F;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin-left: 5vw;
+  font-size: 24px;
+}
+
+.Bottom-Text{
+  margin-left: auto;
+  margin-right: auto;
+}
 
 h1{
   font-size: 31px;
@@ -164,5 +193,11 @@ h1{
   font-weight: 800;
 }
 
+h2{
+  text-align: left;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 14px;
+ 
+}
 
 </style>
