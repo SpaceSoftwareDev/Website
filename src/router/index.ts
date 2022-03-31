8cb2c8ca642578532c9c1b4a8038aab0bc0aad24
mindstormjak <69505539+mindstormjak@users.noreply.github.com>
2022-03-31 10:15:34 +0200

    Updated Styling & Modules

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import { RouteRecordRaw } from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: HomePage
+    component: () => import('@/views/HomePage.vue')
   }
 ]
 
