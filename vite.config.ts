a139190232d4697b9355a2e151f3877655e33531
mindstormjak <69505539+mindstormjak@users.noreply.github.com>
2022-06-15 22:28:58 +0200

    General Updates
    - Removed package-lock.json
    - Updated yarn & dependencies
    - Added 2 vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import * as path from "path"
+import { fileURLToPath, URL } from "url"
+
+import Compression from "vite-plugin-compression"
+import progress from "vite-plugin-progress"
+import colors from "picocolors"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		Compression(),
+		progress({
+			format: `${colors.green(colors.bold("Building"))} ${colors.cyan(
+				":bar"
+			)} :percent`,
+			total: 200,
+			width: 60
+		}),
+	],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src")
+			"@": fileURLToPath(new URL("./src", import.meta.url))
 		}
 	}
 })
