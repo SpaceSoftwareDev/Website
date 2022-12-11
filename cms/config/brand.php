5055cf27d754a4f892c6cf86342397a9e4b82c9f
matomos <martinmojzis06@gmail.com>
2022-12-11 01:55:06 +0100

    feat(cms): setup brand config

---
+++ cms/config/brand.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Specify the default backend brand settings
+    |--------------------------------------------------------------------------
+    |
+    | These parameter values can be overridden by the backend Brand settings
+    |
+    */
+
+    'logoPath'       => '~/storage/logo.png',
+    'faviconPath'    => '~/storage/logo.png',
+    'appName'        => 'space. Website CMS',
+    'tagline'        => 'Complete administration area for space. website',
+    'primaryColor'   => '#34495e',
+    'secondaryColor' => '#0164d7',
+    'accentColor'    => '#3498db',
+    'menuMode'       => 'inline', // options: inline, inline_no_icons, tile, collapse
+    'customLessPath' => null
+];
