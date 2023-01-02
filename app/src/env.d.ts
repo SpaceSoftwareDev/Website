bebbd28c8d16b5e47382538fbe990d9d986586e5
mindstormjak <69505539+mindstormjak@users.noreply.github.com>
2023-01-02 11:15:43 +0100

    chore(app): update dependencies

--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -2,8 +2,11 @@
 
 declare module "*.vue" {
 	import type { DefineComponent } from "vue"
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-	const component: DefineComponent<{}, {}, any>
+	const component: DefineComponent<
+		Record<string, unknown>,
+		Record<string, unknown>,
+		unknown
+	>
 	export default component
 }
 
